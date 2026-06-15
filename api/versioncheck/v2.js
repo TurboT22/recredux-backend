e21543c6f417ee9c07545a60ad735d921485be63
TurboT22 <twritch22@gmail.com>
2026-06-15 16:29:20 -0500

    Update v2.js

--- api/versioncheck/v2.js
+++ api/versioncheck/v2.js
@@ -1,18 +1,23 @@
 export default async function handler(req, res) {
-  // Tell the game client it is safe to communicate with this server
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.status(200).end();
 
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  // This tells the 2023 Rec Room client that your server is online
   return res.status(200).json({
-    "ValidVersion": true,
-    "ServerStatus": "Normal",
-    "Message": "Welcome to your Cloud Revival!",
-    "MaintenanceMode": false
+    "Config": {
+      "api.providers.AppStoreBackend.enabled": false,
+      "api.providers.SteamBackend.enabled": false,
+      "MaintenanceMode": false,
+      "ShowMaintenanceMessage": false,
+      "Matchmaking.Enabled": true
+    },
+    "Features": {
+      "Rooms.CreationEnabled": true,
+      "Logins.Enabled": true,
+      "CustomRooms.Enabled": true,
+      "CrossPlay.Enabled": true
+    },
+    "Status": "Normal"
   });
 }
